2e04e5f4de884d170b486794643c9463b11013ab
Kev-Morel <kpmorel@gmail.com>
2025-08-12 14:46:29 +0100

    cleanup animated text function

--- animatedText.js
+++ animatedText.js
@@ -1,97 +1,26 @@
 const introTextArray = [
-  'H',
-  'He',
-  'Hey',
-  'Hey ',
-  'Hey t',
-  'Hey th',
-  'Hey the',
-  'Hey ther',
-  'Hey there',
-  'Hey there!',
-  'Hey there!',
-  'Hey there!_',
-  'Hey there!_',
-  'Hey there! ',
-  'Hey there! ',
-  'Hey there!_',
-  'Hey there!_',
-  'Hey there! ',
-  'Hey there! ',
-  'Hey there!_',
-  'Hey there!_',
-  'Hey there! ',
-  'Hey there! ',
-  'Hey there!_',
-  'Hey there!_',
-  'Hey there! ',
-  'Hey there! ',
-  'Hey there!_',
-  'Hey there!_',
-  'Hey there!  ',
-  'Hey there!  ',
-  'Hey there! S',
-  'Hey there! Sc',
-  'Hey there! Scr',
-  'Hey there! Scro',
-  'Hey there! Scrol',
-  'Hey there! Scroll',
-  'Hey there! Scroll ',
-  'Hey there! Scroll d',
-  'Hey there! Scroll do',
-  'Hey there! Scroll dow',
-  'Hey there! Scroll down',
-  'Hey there! Scroll down ',
-  'Hey there! Scroll down f',
-  'Hey there! Scroll down fo',
-  'Hey there! Scroll down for',
-  'Hey there! Scroll down for ',
-  'Hey there! Scroll down for m',
-  'Hey there! Scroll down for mo',
-  'Hey there! Scroll down for mor',
-  'Hey there! Scroll down for more',
   'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more ',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more_',
-  'Hey there! Scroll down for more ',
   'Hey there! Scroll down for more ',
 ];
 
 const introAnimation = () => {
   let aniText = document.getElementById('clickMe');
-  let num = 0;
-  console.log('working', aniText);
+  let counter = 0;
+  let numSwitch = 0;
   function writeMe() {
-    if (num > 78) {
+    if (counter > 100) {
       return aniText;
     } else {
-      aniText.innerHTML = introTextArray[num];
-      num++;
-      setTimeout(writeMe, 100);
+      if (counter < 32) {
+        aniText.innerHTML = introTextArray[0].substring(0, counter);
+      } else {
+        aniText.innerHTML = introTextArray[numSwitch];
+        numSwitch === 0 ? numSwitch = 1 : numSwitch = 0;
+      }
     }
+    const timeDelay = counter < 32 ? 80 : 400;
+    counter++;
+    setTimeout(writeMe, timeDelay);
   }
   writeMe(0);
 };
