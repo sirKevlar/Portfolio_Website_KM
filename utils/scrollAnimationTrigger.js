3f7943b26758e1d5f29159064e78c877f2349bee
Kev-Morel <kpmorel@gmail.com>
2025-08-23 10:41:12 +0100

    add scroll animation function

---
+++ utils/scrollAnimationTrigger.js
@@ -0,0 +1,17 @@
+// add .animate class to those HTML elements that you wish to animate. For example, <h1 class="animate">
+// Once in the viewport, the JS will add the 'in-view' class to those elements. For example, <h1 class="animate in-view">
+// Define your CSS to enable animations once that element is in view. For example, h1.in-view { }
+
+document.addEventListener("DOMContentLoaded", () => {
+	const observer = new IntersectionObserver(entries => {
+	  entries.forEach(entry => {
+		if (entry.isIntersecting) {
+		  entry.target.classList.add('in-view');
+		  return;
+		}
+		entry.target.classList.remove('in-view');
+	  });
+	});
+	const allAnimatedElements = document.querySelectorAll('.animate');
+	allAnimatedElements.forEach((element) => observer.observe(element));
+}); 
